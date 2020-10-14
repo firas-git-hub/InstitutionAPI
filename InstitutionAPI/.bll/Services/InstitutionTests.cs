023906df3d22080f0da3c2c6cbbad10d13feff4a
firas mk <firo.mk@hotmail.com>
2020-10-14 18:03:08 +0300

    Add project files.

---
+++ InstitutionAPI/.bll/Services/InstitutionTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstitutionAPI.bll.Services
+{
+    [TestFixture]
+    public class InstitutionTests
+    {
+        [Test]
+        public void CheckPost()
+        {
+            //Assert.
+        }
+
+        [Test]
+        public void CheckGet()
+        {
+
+        }
+    }
+}
---
+++ InstitutionAPI/.dal/institutionContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace InstitutionAPI.Models
+{
+    public class institutionContext : DbContext
+    {
+        public DbSet<institution> institutions { get; set; }
+        public institutionContext(DbContextOptions<institutionContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost; database=master; Trusted_Connection=SSPI; ");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
